58b091d9a7b0ac090fd728eb67203449595e6705
Pedro Tashima <pedrotashima42@gmail.com>
2022-01-13 15:03:28 -0300

    update backup jue 13 ene 2022 15:03:28 -03

--- assets/.config/waybar/scripts/headset-battery.sh
+++ assets/.config/waybar/scripts/headset-battery.sh
@@ -6,15 +6,19 @@
 PERCENT=$(headsetcontrol -b | grep -Eo '[0-9]{1,3}')
 case $((
  $PERCENT >= 0 && $PERCENT <= 20 ? 1 :
- $PERCENT > 20 && $PERCENT <= 40 ? 2 :
- $PERCENT > 40 && $PERCENT <= 60 ? 3 :
- $PERCENT > 6 && $PERCENT <= 80 ? 4 : 5)) in
+ $PERCENT > 20 && $PERCENT <= 30 ? 2 :
+ $PERCENT > 40 && $PERCENT <= 50 ? 3 :
+ $PERCENT > 60 && $PERCENT <= 70 ? 4 :
+ $PERCENT > 70 && $PERCENT <= 80 ? 5 :
+ $PERCENT > 80 && $PERCENT <= 90 ? 6 : 7)) in
 #
-       (1) echo "": $PERCENT%;;
-       (2) echo "": $PERCENT%;;
-       (3) echo "": $PERCENT%;;
-       (4) echo "": $PERCENT%;;
-       (5) echo "": $PERCENT%;;
+       (1) echo "☐": $PERCENT%;;
+       (2) echo "⚀": $PERCENT%;;
+       (3) echo "⚁": $PERCENT%;;
+       (4) echo "⚂": $PERCENT%;;
+       (5) echo "⚃": $PERCENT%;;
+       (6) echo "⚄": $PERCENT%;;
+       (7) echo "⚅": $PERCENT%;;
 esac
 
 
